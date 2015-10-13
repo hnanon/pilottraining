06d38a2f2d04781cade6d4f4207e67c6af7f7785
hnanon <hnanon+1@gmail.com>
2015-10-13 18:38:59 +0000

    First commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, case_sensitive: false
   
   has_many :training_records
   has_many :cockpit_procedures_trainings
